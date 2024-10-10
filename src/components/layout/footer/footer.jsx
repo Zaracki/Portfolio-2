71250b33b2869a3181a346ae6ac39e1ccb7bcb65
Zaracki <119800248+Zaracki@users.noreply.github.com>
2024-10-10 11:19:11 +0200

    Update footer.jsx

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-gray-800 text-center text-white">
       <div className="w-full mx-0 py-4">
-        <p>&copy; Alexander Weidemann</p>
+        <p>Alexander Weidemann</p>
       </div>
     </footer>
   );
